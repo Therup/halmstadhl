f4b447aab9ff7df6f01f9676f67c7b6b8fca9f35
marcus therup <m_therup@hotmail.com>
2024-02-21 14:33:47 +0100

    add high scoring list

--- src/components/teamTable.tsx
+++ src/components/teamTable.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FirebaseService } from "../FirebaseService";
 import {
   Box,
+  Button,
   Paper,
   Table,
   TableBody,
@@ -19,15 +20,23 @@ import images from "../data/images.json";
 import TeamLogo from "./utils/TeamLogo";
 import Admin from "./utils/Admin";
 
+interface GoalScorer {
+  player: string;
+  goals: number;
+}
+
 const TeamTable: React.FC = () => {
   const { user } = useUser();
   const [teams, setTeams] = useState<{ name: string; statistics: any }[]>([]);
+  const [goalScorers, setGoalScorers] = useState<GoalScorer[]>([]);
+  const [showTable, setShowTable] = useState(true); // State för att hålla reda på vilket läge som ska visas
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const matchesData = await FirebaseService.getMatches();
         const teamStatisticsMap = new Map<string, any>();
+        const allGoalScorers: GoalScorer[] = [];
 
         // Filtrera bort matcher där isPlayed är false
         const playedMatches = matchesData.filter((match) => match.isPlayed);
@@ -45,7 +54,25 @@ const TeamTable: React.FC = () => {
             match.result.awayScore,
             match.result.homeScore
           );
+          allGoalScorers.push(
+            ...match.homeGoalScorers,
+            ...match.awayGoalScorers
+          );
         });
+        //Kombinera ihop målskyttar och räkna upp antal mål
+        const combinedGoalScorers: GoalScorer[] = [];
+        allGoalScorers.forEach((scorer) => {
+          const existingScorerIndex = combinedGoalScorers.findIndex(
+            (existingScorer) => existingScorer.player === scorer.player
+          );
+          if (existingScorerIndex !== -1) {
+            combinedGoalScorers[existingScorerIndex].goals += scorer.goals;
+          } else {
+            combinedGoalScorers.push(scorer);
+          }
+        });
+
+        setGoalScorers(combinedGoalScorers);
 
         const teamsData = Array.from(teamStatisticsMap.values());
 
@@ -108,104 +135,144 @@ const TeamTable: React.FC = () => {
         alignItems: "center",
       }}
     >
-      <PageTitle title="Tabell 23/24" icon={<EqualizerIcon />} />
-      <TableContainer
-        component={Paper}
-        style={{ display: "inline-block", maxWidth: "600px" }}
-      >
-        <Table aria-label="simple table">
-          <TableHead style={{ backgroundColor: "rgb(0, 44, 81)" }}>
-            <TableRow>
-              <TableCell
-                style={{
-                  fontWeight: "bold",
-                  color: "white",
-                  padding: 5,
-                  width: "40%",
-                }}
-                padding="none"
-              >
-                Team
-              </TableCell>
-              <TableCell
-                style={{ fontWeight: "bold", color: "white" }}
-                padding="none"
-              >
-                GP
-              </TableCell>
-              <TableCell
-                style={{ fontWeight: "bold", color: "white" }}
-                padding="none"
-              >
-                WI
-              </TableCell>
-              <TableCell
-                style={{ fontWeight: "bold", color: "white" }}
-                padding="none"
-              >
-                LO
-              </TableCell>
-              <TableCell
-                style={{ fontWeight: "bold", color: "white" }}
-                padding="none"
-              >
-                TI
-              </TableCell>
-              <TableCell
-                style={{ fontWeight: "bold", color: "white" }}
-                padding="none"
-              >
-                GF
-              </TableCell>
-              <TableCell
-                style={{ fontWeight: "bold", color: "white" }}
-                padding="none"
-              >
-                GA
-              </TableCell>
-              <TableCell
-                style={{ fontWeight: "bold", color: "white" }}
-                padding="none"
-              >
-                PO
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {teams.map((team, index) => (
-              <TableRow key={index}>
-                <TableCell
-                  padding="none"
-                  style={{ padding: 5, fontWeight: "bold" }}
-                >
-                  <Box style={{ display: "flex" }}>
-                    <Box style={{ marginRight: "5px" }}>
-                      <TeamLogo teamName={team.name} size={50} />
-                    </Box>
-                    <Box style={{ marginTop: "17px" }}>{team.name}</Box>
-                  </Box>
-                </TableCell>
-                <TableCell padding="none">
-                  {team.statistics.playedMatches}
-                </TableCell>
-                <TableCell padding="none">{team.statistics.wins}</TableCell>
-                <TableCell padding="none">{team.statistics.losses}</TableCell>
-                <TableCell padding="none">{team.statistics.draws}</TableCell>
-                <TableCell padding="none">
-                  {team.statistics.goalsScored}
-                </TableCell>
-                <TableCell padding="none">
-                  {team.statistics.goalsConceded}
-                </TableCell>
-                <TableCell padding="none" style={{ fontWeight: "bold" }}>
-                  {team.statistics.points}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {/* Knappar för att byta mellan tabell och skytteliga */}
+      <Box style={{ marginBottom: "20px" }}>
+        <Button
+          variant="contained"
+          style={{ marginRight: "10px", backgroundColor: "white" }}
+          onClick={() => setShowTable(true)}
+        >
+          Visa tabell
+        </Button>
+        <Button
+          variant="contained"
+          style={{ backgroundColor: "white" }}
+          onClick={() => setShowTable(false)}
+        >
+          Visa skytteliga
+        </Button>
+      </Box>
 
+      {showTable ? (
+        <>
+          <PageTitle title="Tabell 23/24" icon={<EqualizerIcon />} />
+          <TableContainer
+            component={Paper}
+            style={{ display: "inline-block", maxWidth: "600px" }}
+          >
+            <Table aria-label="simple table">
+              <TableHead style={{ backgroundColor: "rgb(0, 44, 81)" }}>
+                <TableRow>
+                  <TableCell
+                    style={{
+                      fontWeight: "bold",
+                      color: "white",
+                      padding: 5,
+                      width: "40%",
+                    }}
+                    padding="none"
+                  >
+                    Team
+                  </TableCell>
+                  <TableCell
+                    style={{ fontWeight: "bold", color: "white" }}
+                    padding="none"
+                  >
+                    GP
+                  </TableCell>
+                  <TableCell
+                    style={{ fontWeight: "bold", color: "white" }}
+                    padding="none"
+                  >
+                    WI
+                  </TableCell>
+                  <TableCell
+                    style={{ fontWeight: "bold", color: "white" }}
+                    padding="none"
+                  >
+                    LO
+                  </TableCell>
+                  <TableCell
+                    style={{ fontWeight: "bold", color: "white" }}
+                    padding="none"
+                  >
+                    TI
+                  </TableCell>
+                  <TableCell
+                    style={{ fontWeight: "bold", color: "white" }}
+                    padding="none"
+                  >
+                    GF
+                  </TableCell>
+                  <TableCell
+                    style={{ fontWeight: "bold", color: "white" }}
+                    padding="none"
+                  >
+                    GA
+                  </TableCell>
+                  <TableCell
+                    style={{ fontWeight: "bold", color: "white" }}
+                    padding="none"
+                  >
+                    PO
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {teams.map((team, index) => (
+                  <TableRow key={index}>
+                    <TableCell
+                      padding="none"
+                      style={{ padding: 5, fontWeight: "bold" }}
+                    >
+                      <Box style={{ display: "flex" }}>
+                        <Box style={{ marginRight: "5px" }}>
+                          <TeamLogo teamName={team.name} size={50} />
+                        </Box>
+                        <Box style={{ marginTop: "17px" }}>{team.name}</Box>
+                      </Box>
+                    </TableCell>
+                    <TableCell padding="none">
+                      {team.statistics.playedMatches}
+                    </TableCell>
+                    <TableCell padding="none">{team.statistics.wins}</TableCell>
+                    <TableCell padding="none">
+                      {team.statistics.losses}
+                    </TableCell>
+                    <TableCell padding="none">
+                      {team.statistics.draws}
+                    </TableCell>
+                    <TableCell padding="none">
+                      {team.statistics.goalsScored}
+                    </TableCell>
+                    <TableCell padding="none">
+                      {team.statistics.goalsConceded}
+                    </TableCell>
+                    <TableCell padding="none" style={{ fontWeight: "bold" }}>
+                      {team.statistics.points}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      ) : (
+        <Box>
+          <PageTitle title="Skytteliga 23/24" icon={<EqualizerIcon />} />
+          <ol>
+            {/* Visa top 10 målskyttar */}
+            {goalScorers
+              .sort((a, b) => b.goals - a.goals)
+              .slice(0, 10)
+              .map((scorer, index) => (
+                <li key={index}>
+                  {scorer.player} - {scorer.goals} mål
+                </li>
+              ))}
+          </ol>
+        </Box>
+      )}
       <Admin />
       {user && user.isAdmin && <AddTeamForm />}
       <Box style={{ marginTop: "20px" }}>
